0b1e3340a77185c837fcfb4f81975fa0df513821
thekill022 <gibfathoni@gmail.com>
2024-04-04 12:17:29 +0800

    inisialisasi variabel i sebagai variabel global

--- AlgorithmaSearch_0174/AlgorithmaSearch_0174.cpp
+++ AlgorithmaSearch_0174/AlgorithmaSearch_0174.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int arr[20]; //inisialisasi variabel arr sebagai variabel global untuk menampung isi data
 int n; //inisialisasi variabel n sebagai variabel global untuk menentukan panjang data array
+int i; //inisialisasi variabel i sebagai variabel global untuk menandai index dari array
 
 int main()
 {
