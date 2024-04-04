e806285e3c7e07052242ba9041d92e7e962e2fd4
thekill022 <gibfathoni@gmail.com>
2024-04-04 12:18:06 +0800

    membuat prosedur input

--- AlgorithmaSearch_0174/AlgorithmaSearch_0174.cpp
+++ AlgorithmaSearch_0174/AlgorithmaSearch_0174.cpp
@@ -5,6 +5,11 @@ int arr[20]; //inisialisasi variabel arr sebagai variabel global untuk menampung
 int n; //inisialisasi variabel n sebagai variabel global untuk menentukan panjang data array
 int i; //inisialisasi variabel i sebagai variabel global untuk menandai index dari array
 
+void input() //membuat prosedur input
+{
+
+}
+
 int main()
 {
 	
